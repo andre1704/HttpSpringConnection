35305e4922ff59493316aa72e4fd6f3a37606a2b
andre1704 <andrzejslowinski93@gmail.com>
2017-05-08 20:57:55 +0200

    1.1

--- app/src/main/java/com/sdacademy/slowinski/andrzej/httpspringconnection/Adapter.java
+++ app/src/main/java/com/sdacademy/slowinski/andrzej/httpspringconnection/Adapter.java
@@ -6,8 +6,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.sdacademy.slowinski.andrzej.httpspringconnection.model.Task;
@@ -27,30 +29,89 @@ import static com.sdacademy.slowinski.andrzej.httpspringconnection.mapper.TaskMa
 
 public class Adapter extends ArrayAdapter<Task> {
     private TaskService taskService;
-    private boolean EditMode;
-    public Adapter(Context context, List<Task> objects, TaskService taskService) {
-        super(context, 0, objects);
-        this.taskService=taskService;
-    }
-    
-    public View getView(int position, View convertView, ViewGroup parent){
-        final Task item=  getItem(position);
-        if(convertView==null){
-            convertView= LayoutInflater.from(getContext()).inflate(R.layout.single_row,parent,false);
-        }
-        TextView itemNo= (TextView) convertView.findViewById(R.id.singleRowNo);
-        TextView itemText= (TextView) convertView.findViewById(R.id.singleRowText);
-        final CheckBox itemCheckBox= (CheckBox) convertView.findViewById(R.id.itemCHeckBox);
-        checboxListener(item, itemCheckBox);
 
-        itemNo.setText(String.valueOf(position));
-        itemText.setText(item.getValue());
-        itemCheckBox.setChecked(item.getCompleted());
+    public int getEditMode() {
+        return editMode;
+    }
+
+    public void setEditMode(int editMode) {
+        this.editMode = editMode;
+    }
+
+    private int editMode = -1;
+
+    public Adapter(Context context, List<Task> objects, TaskService taskService, int editMode) {
+        super(context, 0, objects);
+        this.taskService = taskService;
+        this.editMode = editMode;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final Task item = getItem(position);
+        if (convertView == null) {
+            if (editMode == position) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.single_row_edit, parent, false);
+            } else {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.single_row, parent, false);
+            }
+        }
+        if (editMode != position) {
+            TextView itemNo = (TextView) convertView.findViewById(R.id.singleRowNo);
+            TextView itemText = (TextView) convertView.findViewById(R.id.singleRowText);
+            final CheckBox itemCheckBox = (CheckBox) convertView.findViewById(R.id.itemCHeckBox);
+
+            checkboxListener(item, itemCheckBox);
+
+            itemNo.setText(String.valueOf(position));
+            itemText.setText(item.getValue());
+            itemCheckBox.setChecked(item.getCompleted());
+        } else {
+
+
+            TextView itemNoE = (TextView) convertView.findViewById(R.id.singleRowNoEdit);
+            TextView itemTextE = (TextView) convertView.findViewById(R.id.singleRowTextEdit);
+            EditText itemTextEdit = (EditText) convertView.findViewById(R.id.edited_field);
+            final CheckBox itemCheckBoxE = (CheckBox) convertView.findViewById(R.id.itemCHeckBoxEdit);
+
+            checkboxListener(item, itemCheckBoxE);
+
+            Button button = (Button) convertView.findViewById(R.id.save_editedEdit);
+            itemNoE.setText(String.valueOf(position));
+            itemTextE.setText(item.getValue());
+            itemCheckBoxE.setChecked(item.getCompleted());
+
+        }
+
 
         return convertView;
     }
 
-    private void checboxListener(final Task item, final CheckBox itemCheckBox) {
+    private void buttonListener(final Task item, Button button, final EditText editText) {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Call<TaskDTO> call = taskService.updateTask(taskToTaskDTO(item));
+                call.enqueue(new Callback<TaskDTO>() {
+                    @Override
+                    public void onResponse(Call<TaskDTO> call, Response<TaskDTO> response) {
+                        TaskDTO received = response.body();
+                        item.setValue(String.valueOf(editText.getText()));
+                        notifyDataSetChanged();
+                        Log.d("add_checked_status", "successed add checked status");
+                    }
+
+                    @Override
+                    public void onFailure(Call<TaskDTO> call, Throwable t) {
+                        Log.d("add_checked_status", "failure add checked status");
+                    }
+                });
+            }
+        });
+
+    }
+
+    private void checkboxListener(final Task item, final CheckBox itemCheckBox) {
         itemCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
--- app/src/main/java/com/sdacademy/slowinski/andrzej/httpspringconnection/MainActivity.java
+++ app/src/main/java/com/sdacademy/slowinski/andrzej/httpspringconnection/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.sdacademy.slowinski.andrzej.httpspringconnection.model.Task;
 import com.sdacademy.slowinski.andrzej.httpspringconnection.service.TaskService;
@@ -33,79 +34,87 @@ public class MainActivity extends AppCompatActivity {
     private TaskService taskService;
     private Button button;
     private List<TaskDTO> taskDTOs;
-    private final  String BASE_URL="https://shrouded-fjord-81597.herokuapp.com/";
+    private final String BASE_URL = "https://shrouded-fjord-81597.herokuapp.com/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tasks=new ArrayList<>();
-        taskDTOs=new ArrayList<>();
+        tasks = new ArrayList<>();
+        taskDTOs = new ArrayList<>();
         editText = (EditText) findViewById(R.id.edit);
-        listView= (ListView) findViewById(R.id.list);
-        button= (Button) findViewById(R.id.button);
-        Retrofit retrofit=new Retrofit.Builder()
+        listView = (ListView) findViewById(R.id.list);
+        button = (Button) findViewById(R.id.button);
+        Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
-        taskService=retrofit.create(TaskService.class);
-        adapter=new Adapter(this,tasks,taskService);
+        taskService = retrofit.create(TaskService.class);
+        adapter = new Adapter(this, tasks, taskService, -1);
         listView.setAdapter(adapter);
-        Call<List<TaskDTO>> call =taskService.findAllTaskByUser((long) 30);
+        Call<List<TaskDTO>> call = taskService.findAllTaskByUser((long) 30);
         call.enqueue(new Callback<List<TaskDTO>>() {
             @Override
             public void onResponse(Call<List<TaskDTO>> call, Response<List<TaskDTO>> response) {
-                for(int i=0;i<response.body().size();i++){
+                for (int i = 0; i < response.body().size(); i++) {
                     tasks.add(taskDTOTotask(response.body().get(i)));
                 }
                 adapter.notifyDataSetChanged();
-                Log.d("successLog","success download values from the server");
+                Log.d("successLog", "success download values from the server");
             }
 
             @Override
             public void onFailure(Call<List<TaskDTO>> call, Throwable t) {
-                Log.d("failLog","fail to download values from the server");
+                Log.d("failLog", "fail to download values from the server");
             }
         });
+        switchToEditMode();
         removeRecord();
     }
+
+    private void switchToEditMode() {
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("click", "click");
+
+                listView.setAdapter(new Adapter(MainActivity.this, tasks, taskService, position));
+            }
+        });
+    }
+
 
     private void removeRecord() {
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
-                final Task task= tasks.get(position);
+                final Task task = tasks.get(position);
                 taskService.deleteTask(task.getId()).enqueue(new Callback<Void>() {
                     @Override
                     public void onResponse(Call<Void> call, Response<Void> response) {
                         tasks.remove(position);
-                        Log.d("successLog","success delete record from the server");
+                        Log.d("successLog", "success delete record from the server");
                         adapter.notifyDataSetChanged();
                     }
 
                     @Override
                     public void onFailure(Call<Void> call, Throwable t) {
-                        Log.d("failLog","fail to delete record from the server");
+                        Log.d("failLog", "fail to delete record from the server");
                     }
                 });
-
                 return false;
             }
         });
     }
 
-    public void onClickEdit(View view){
 
-    }
-
-
-    public void onClick(View view){
+    public void onClick(View view) {
         final Task[] task = {new Task(null, editText.getText().toString(), false)};
 
         taskService.updateTask(taskToTaskDTO(task[0])).enqueue(new Callback<TaskDTO>() {
             @Override
             public void onResponse(Call<TaskDTO> call, Response<TaskDTO> response) {
-                task[0] =taskDTOTotask(response.body());
+                task[0] = taskDTOTotask(response.body());
                 tasks.add(task[0]);
                 Log.d("add_record_log", "successed add new record");
                 adapter.notifyDataSetChanged();
